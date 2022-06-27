6e8f384298e6af01384ec366e719b3f510f5a395
TannisthaBiswas <97463197+TannisthaBiswas@users.noreply.github.com>
2022-06-27 12:57:44 +0530

    Update carList.js
    original

--- pages/data/carList.js
+++ pages/data/carList.js
@@ -1,4 +1,4 @@
- const carList = 
+ export const carList = [
   {
     imgUrl: 'https://i.ibb.co/cyvcpfF/uberx.png',
     service: 'UberX',
@@ -23,5 +23,5 @@
     imgUrl: ' https://i.ibb.co/1nStPWT/uberblacksuv.png',
     service: 'Black SUV',
     multiplier: 2.8,
-  }
-export default carList
+  }]
+
